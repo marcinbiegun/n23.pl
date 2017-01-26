c6e2e7ff26de424ad6cbb5f71df43e4f5a7cd7e3
Marcin Biegun <marcin@marcinbiegun.com>
2017-01-26 22:11:45 +0100

    Update domain name

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 jekyll build
 rm _site.tar.gz
 tar -czvf _site.tar.gz _site
-ssh $USER@marcinbiegun.com "rm ~/_site.tar.gz"
-scp _site.tar.gz $USER@marcinbiegun.com:~
-ssh $USER@marcinbiegun.com "rm -rf ~/_marcinbiegun.com; tar -xvzf _site.tar.gz; mv _site _marcinbiegun.com"
-ssh $USER@marcinbiegun.com "rm -rf ~/marcinbiegun.com; mv _marcinbiegun.com marcinbiegun.com"
-ssh $USER@marcinbiegun.com "rm ~/_site.tar.gz"
+ssh $USER@n23.pl "rm ~/_site.tar.gz"
+scp _site.tar.gz $USER@n23.pl:~
+ssh $USER@n23.pl "rm -rf ~/_n23.pl; tar -xvzf _site.tar.gz; mv _site _n23.pl"
+ssh $USER@n23.pl "rm -rf ~/n23.pl; mv _n23.pl n23.pl"
+ssh $USER@n23.pl "rm ~/_site.tar.gz"
