fe0a9c6401369634368ca93903f5ad5038973d41
Marcin Biegun <marcin@mm.st>
2018-06-20 11:36:37 +0200

    Remove deploy script

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-jekyll build
-rm _site.tar.gz
-tar -czvf _site.tar.gz _site
-ssh $USER@n23.pl "rm ~/_site.tar.gz"
-scp _site.tar.gz $USER@n23.pl:~
-ssh $USER@n23.pl "rm -rf ~/_n23.pl; tar -xvzf _site.tar.gz; mv _site _n23.pl"
-ssh $USER@n23.pl "rm -rf ~/n23.pl; mv _n23.pl n23.pl"
-ssh $USER@n23.pl "rm ~/_site.tar.gz"
